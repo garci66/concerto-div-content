145b9cf9e865c4dea0036da5fdd42028c8cd5f8c
Diego Garcia del Rio <garci66@gmail.com>
2017-03-14 22:01:29 -0300

    Sort routes issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :divcontents, :controller => :contents, :except => [:index, :show], :path => "content"
+  resources :div_contents, :controller => :contents, :except => [:index, :show], :path => "content"
 end
